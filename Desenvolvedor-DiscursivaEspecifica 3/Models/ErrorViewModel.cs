95087ce8eb13ec768b183de12d50343c5e9a4469
Lucas Samel <lucas.samel97@gmail.com>
2020-09-20 18:21:32 -0300

    Add project files.

---
+++ Desenvolvedor-DiscursivaEspecifica 3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Desenvolvedor_DiscursivaEspecifica_3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
