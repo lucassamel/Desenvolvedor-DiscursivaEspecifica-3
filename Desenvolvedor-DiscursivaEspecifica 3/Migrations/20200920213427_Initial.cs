390a049cfd5c5fb19b36abfa969633684e0c694b
Lucas Samel <lucas.samel97@gmail.com>
2020-09-20 18:45:30 -0300

    Migrations

---
+++ Desenvolvedor-DiscursivaEspecifica 3/Migrations/20200920213427_Initial.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Desenvolvedor_DiscursivaEspecifica_3.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    CursoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.CursoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turma",
+                columns: table => new
+                {
+                    TurmaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    CursoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turma", x => x.TurmaId);
+                    table.ForeignKey(
+                        name: "FK_Turma_Curso_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Curso",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bloco",
+                columns: table => new
+                {
+                    BlocoId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    TurmaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bloco", x => x.BlocoId);
+                    table.ForeignKey(
+                        name: "FK_Bloco_Turma_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turma",
+                        principalColumn: "TurmaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Disciplina",
+                columns: table => new
+                {
+                    DisciplinaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    BlocoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Disciplina", x => x.DisciplinaId);
+                    table.ForeignKey(
+                        name: "FK_Disciplina_Bloco_BlocoId",
+                        column: x => x.BlocoId,
+                        principalTable: "Bloco",
+                        principalColumn: "BlocoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bloco_TurmaId",
+                table: "Bloco",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Disciplina_BlocoId",
+                table: "Disciplina",
+                column: "BlocoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Turma_CursoId",
+                table: "Turma",
+                column: "CursoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Disciplina");
+
+            migrationBuilder.DropTable(
+                name: "Bloco");
+
+            migrationBuilder.DropTable(
+                name: "Turma");
+
+            migrationBuilder.DropTable(
+                name: "Curso");
+        }
+    }
+}
